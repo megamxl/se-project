b853ef647b727aa5a7f6164076c9ec613fbfe8ad
Megamaxl <maximilian.nowak@outlook.at>
2025-03-14 14:02:12 +0100

    added second enapoint and fixed naming

--- Currency-Converter-Server/currency.converter/src/test/java/com/prestige/wheels/currency/converter/ApplicationTests.java
+++ Currency-Converter-Server/currency.converter/src/test/java/com/prestige/wheels/currency/converter/ApplicationTests.java
@@ -7,10 +7,12 @@ import com.prestige.wheels.currency.converter.soap.model.Currency;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Autowired
