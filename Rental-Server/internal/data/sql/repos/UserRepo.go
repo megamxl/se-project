0e1b8137192bd0df4c2658326bf9c7d1fc940276
Heinz Schweitzer <104928904+heinzSchw@users.noreply.github.com>
2025-03-23 12:40:16 +0100

    User services (#7)
    * Initial setup rental service and soap call (#5)
    
    * Added docker and data init
    
    * open api changes
    
    * Api gen v1
    
    * Api gen v1
    
    * test Interceptor
    
    * basic structure
    
    * Interfaces
    
    * Soap Integration v1
    
    * all soap methods
    
    * add user service + test
    
    * add user service + tests + api impl
    
    ---------
    
    Co-authored-by: Maximilian Nowak <78011818+megamxl@users.noreply.github.com>

--- Rental-Server/internal/data/sql/repos/UserRepo.go
+++ Rental-Server/internal/data/sql/repos/UserRepo.go
@@ -10,6 +10,11 @@ var _ dataInt.UserRepository = (*UserRepo)(nil)
 type UserRepo struct {
 }
 
+func (u UserRepo) GetAllUsers() ([]dataInt.RentalUser, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
 func (u UserRepo) GetUserByEmail(email string) (dataInt.RentalUser, error) {
 	//TODO implement me
 	panic("implement me")
