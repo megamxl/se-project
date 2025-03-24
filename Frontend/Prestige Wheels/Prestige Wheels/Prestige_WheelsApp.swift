3cff47c39249049762e469c0ff0e60d3f163b28a
Michael Luegmayer <michael.luegmayer@me.com>
2025-03-24 20:16:07 +0100

    create Xcode Project

---
+++ Frontend/Prestige Wheels/Prestige Wheels/Prestige_WheelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prestige_WheelsApp.swift
+//  Prestige Wheels
+//
+//  Created by Michael Luegmayer on 24.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Prestige_WheelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
