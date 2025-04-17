26caf398f94cda37b8049256bb23fb750725836a
heinzSchw <heinz.schweitzer@icloud.com>
2025-04-17 18:30:16 +0200

    rebase with master and change to microservice version

--- Frontend/GeneratedMoyaClient/OpenAPIClient/Classes/OpenAPIs/APIs.swift
+++ Frontend/GeneratedMoyaClient/OpenAPIClient/Classes/OpenAPIs/APIs.swift
@@ -9,7 +9,7 @@ import Foundation
 import FoundationNetworking
 #endif
 open class OpenAPIClientAPI {
-    public static var basePath = "http://localhost:8098"
+    public static var basePath = "http://localhost"
     public static var customHeaders: [String: String] = [:]
     public static var credential: URLCredential?
     public static var requestBuilderFactory: RequestBuilderFactory = URLSessionRequestBuilderFactory()
